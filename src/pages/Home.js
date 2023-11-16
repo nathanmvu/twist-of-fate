4f25dbb85b06f4edf55c3c1c8581f27414a3b275
Kavya <kavya.aswadhati@gmail.com>
2023-11-15 19:49:09 -0800

    Added an ascii art header, attempting to format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import  title_image from '../assets/ascii_twist_of_fate.png';
+// This import isn't working
+// TODO: react/js formatting conventions?
+import './CenteredImage.css'; 
+
+// TODO: turn into marquee, have text constantly wrap?
 import HorizontalScrollingTextBar from './HorizontalScrollingTextBar';
 
 
 const Home = () => {
   const text = ["a twist of fate has brought you here, where will u go next?"];
-
   return (
     <div>
-      <h1>Hello World</h1>
-      <HorizontalScrollingTextBar texts={text} />
+      <img
+        src={title_image}
+        alt="ascii title"
+        className="centered-image"
+      />
+      <HorizontalScrollingTextBar texts={text} />       
     </div>
-    
-
   )
 }
 
